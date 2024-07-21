fe76d23084be26ac8ae5fac31dc0c79e4fcc6322
haris-hm <haris.mehuljic@drake.edu>
2024-07-21 15:45:46 -0500

    Initial commit

---
+++ src/main/java/com/harismehuljic/billboards/Billboards.java
@@ -0,0 +1,10 @@
+package com.harismehuljic.billboards;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Billboards implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
